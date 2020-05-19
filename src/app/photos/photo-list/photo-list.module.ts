4a8334953c923f50f416f34a74d4f1c9031f75fd
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-05-19 13:05:00 -0300

    Made Search component

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
         FilterByDescriptionPipe,
         PhotoListComponent,
         PhotosComponent,
-        LoadButtonComponent
+        LoadButtonComponent,
+        SearchComponent
     ],
     imports: [
         CommonModule,
