b2039dad61c6f2a830057aab91367acfa54542f2
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-05-21 18:20:58 -0300

    Sinup page + validators

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
     imports: [
+        FormsModule,
         ReactiveFormsModule,
         CommonModule,
         VMessageModule,
         RouterModule
     ],
-    declarations: [ SignInComponent ]
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent
+    ]
 })
 export class HomeModule {}
