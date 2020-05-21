368fc7ea1e2d6e7bcc60ea20e1024ec169bf4fd5
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-05-21 19:38:41 -0300

    Async Validator -> userNameIsTaken

---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { SignUpService } from './signup.service';
+import { AbstractControl } from "@angular/forms";
+import { debounceTime, switchMap, map, first } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class UserNotTakenValidatorService {
+
+    constructor(private signupService: SignUpService){}
+
+    userNotTakenValidator() {
+        return (control: AbstractControl) => {
+            return control.valueChanges
+                .pipe(debounceTime(300))
+                .pipe(switchMap(user => this.signupService.userNameIsTaken(user)))
+                .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
+                .pipe(first());
+        };
+    }
+}
