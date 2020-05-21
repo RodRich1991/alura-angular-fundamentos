f6250269bc18252b625ac32f39d74a8d0a9f2548
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-05-21 17:47:00 -0300

    Router link to login

--- src/app/core/coremodule.ts
+++ src/app/core/coremodule.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [ CommonModule ],
+    imports: [ 
+        CommonModule,
+        RouterModule
+    ],
     declarations: [ HeaderComponent ],
     exports: [ HeaderComponent ]
 })
