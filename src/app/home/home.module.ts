c0d69f0e8da185f6b39e3ea16d7f42ccd3693645
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-05-21 13:57:45 -0300

    Login redirect

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
         ReactiveFormsModule,
         CommonModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ],
     declarations: [ SignInComponent ]
 })
