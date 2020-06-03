984672c5777a80936b9283425d6d84d52140c0c2
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-06-03 18:11:35 -0300

    Footer component

--- src/app/core/coremodule.ts
+++ src/app/core/coremodule.ts
@@ -4,14 +4,21 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RequestInterceptor } from './auth/request.interceptor';
+import { FooterComonent } from "./footer/footer.component";
 
 @NgModule({
     imports: [ 
         CommonModule,
         RouterModule
     ],
-    declarations: [ HeaderComponent ],
-    exports: [ HeaderComponent ],
+    declarations: [ 
+        HeaderComponent,
+        FooterComonent
+    ],
+    exports: [ 
+        HeaderComponent,
+        FooterComonent
+    ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true }
     ]
