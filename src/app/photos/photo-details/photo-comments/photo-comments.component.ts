14d4d5b0b6a2a2523f40db20fdb793e2a5ec8261
Rodrigo Costa <rodrigo.udi_costa@hotmail.com>
2020-07-06 14:46:22 -0300

    Add comments & refresh

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -3,6 +3,7 @@ import { PhotoService } from '../../photo/photo.service';
 import { Observable } from 'rxjs';
 import { PhotoComment } from '../../photo/photo-comment';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'ap-photo-comments',
@@ -25,6 +26,16 @@ export class PhotoCommentsComponent implements OnInit {
         this.commentForm = this.fb.group({
             comment: ['', Validators.maxLength(300)]
         });
+    }
+
+    save() {
+        const comment = this.commentForm.get('comment').value as string;
+        this.comments$ = this.photoService
+            .addComment(this.photoId, comment)
+            .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+            .pipe(tap(() => {
+                this.commentForm.reset();
+            }));
     }
 
 }
